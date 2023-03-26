bff5271b2a8835c1bbc2421c03153a2afed4a92c
katiepo456 <kathryn.james@mymail.champlain.edu>
2023-03-26 17:11:09 -0400

    fishing line
    attempt at creating the fishing line, still doesn't work because I can't figure out how to lock it on an absolute point and follow the sprite

--- Fishing Game - Godot/Player.gd
+++ Fishing Game - Godot/Player.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 const SPEED = 750 # how fast the player will move (pixels/sec)
-const FISHING_LINE = 2*(OS.window_size.x / 3) # 2/3 of window width
+const FISHING_LINE_X = 2*(OS.window_size.x / 3) # 2/3 of window width
+const FISHING_LINE_Y = 3*(OS.window_size.y / 4) # 3/4 of window height
 
 # The fish hook follows the height of the mouse cursor.
 func _process(delta):
@@ -10,4 +11,15 @@ func _process(delta):
 	# normalize it and multiply by time and speed
 	vec = vec.normalized() * delta * SPEED
 	position += vec # move by that vector
-	position.x = FISHING_LINE # locks on vertical axis of screen
+	position.x = FISHING_LINE_X # locks on vertical axis of screen
+	update()
+
+func _draw():
+	var from = Vector2(0, 0)
+	var to = Vector2(0, -(FISHING_LINE_Y))
+	
+	from = from.normalized() * 100
+	to = to.normalized() * 100
+	
+	# black
+	draw_line(from, to, Color(0, 0, 0), 5)
