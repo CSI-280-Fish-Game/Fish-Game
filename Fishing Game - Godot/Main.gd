9b523efa584d849675c646a4df312e46a72343bd
katiepo456 <kathryn.james@mymail.champlain.edu>
2023-04-23 13:43:29 -0400

    attempt at fixing mob path - it didn't work
    I tried fixing the Path2D to actually have the fish follow different paths. I'm not sure why but no matter how I edit the current path, the fish continue to only go on a straight line.

--- Fishing Game - Godot/Main.gd
+++ Fishing Game - Godot/Main.gd
@@ -4,56 +4,28 @@ extends Node
 export var mob_scene: PackedScene
 var score
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	new_game()
 
 func new_game():
-	# $Player.start($StartPosition.position)
 	$StartTimer.start()
 
 func _on_MobTimer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instance()
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
 
-	# Choose a random location on Path2D.
-	var path_node = randi()%5+1
-	var mob_spawn_location
-	if path_node == 1:
-		mob_spawn_location = get_node("MobPath/MobSpawnLocation")
-		mob_spawn_location.offset = randf()
-	elif path_node == 2:
-		mob_spawn_location = get_node("MobPath2/MobSpawnLocation")
-		mob_spawn_location.offset = randf()
-	elif path_node == 3:
-		mob_spawn_location = get_node("MobPath3/MobSpawnLocation")
-		mob_spawn_location.offset = randf()
-	elif path_node == 4:
-		mob_spawn_location = get_node("MobPath4/MobSpawnLocation")
-		mob_spawn_location.offset = randf()
-	else:
-		mob_spawn_location = get_node("MobPath5/MobSpawnLocation")
-		mob_spawn_location.offset = randf()
-
-	# Set the mob's direction perpendicular to the path direction.
-	# var direction = mob_spawn_location.rotation + PI / 2
-
-	# Set the mob's position to a random location.
+	# Set the mob's position to the spawn location.
 	mob.position = mob_spawn_location.position
-
-	# Add some randomness to the direction.
-	#direction += rand_range(-PI / 4, PI / 4)
-	#direction -= 1000
+	print("Here I am!")
 	
-
 	# Choose the velocity for the mob.
-	var velocity = Vector2(rand_range(125.0, 225.0), 0.0)
-	mob.linear_velocity = velocity.rotated(245)
+	var velocity = Vector2(rand_range(125.0, 200.0), 0.0)
+	mob.linear_velocity = velocity
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
-
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
