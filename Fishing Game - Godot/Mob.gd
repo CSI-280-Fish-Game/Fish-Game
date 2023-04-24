c2099a4540a9c92a1ace2ee91bc300fc60d37769
katiepo456 <kathryn.james@mymail.champlain.edu>
2023-04-24 17:14:11 -0400

    fish sprite variety

--- Fishing Game - Godot/Mob.gd
+++ Fishing Game - Godot/Mob.gd
@@ -7,8 +7,8 @@ var score = 0
 # Called when mob node enters scene for first time.
 func _ready():
 	$AnimatedSprite.play()
-	#var mob_types = $AnimatedSprite.get_sprite_frames().get_animation_names()
-	#$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]  # randomly picks animation type
+	var mob_types = $AnimatedSprite.get_sprite_frames().get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]  # randomly picks animation type
 
 # Destroys mob instance when it leaves screen view.
 func _on_VisibilityNotifier2D_screen_exited() -> void:
