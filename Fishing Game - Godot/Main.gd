6511108a875704f4621afe060d033a46a3cb8960
katiepo456 <kathryn.james@mymail.champlain.edu>
2023-04-24 16:28:53 -0400

    different paths for the mob!!!
    literally had to change .position to .global_position - Godot makes me angry :(((

--- Fishing Game - Godot/Main.gd
+++ Fishing Game - Godot/Main.gd
@@ -15,9 +15,23 @@ func _on_MobTimer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instance()
 	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	
+	# Choose a random location on Path2D.
+	var path_node = randi()%5+1
+	print(path_node)
+	if path_node == 1:
+		mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	elif path_node == 2:
+		mob_spawn_location = get_node("MobPath2/MobSpawnLocation")
+	elif path_node == 3:
+		mob_spawn_location = get_node("MobPath3/MobSpawnLocation")
+	elif path_node == 4:
+		mob_spawn_location = get_node("MobPath4/MobSpawnLocation")
+	else:
+		mob_spawn_location = get_node("MobPath5/MobSpawnLocation")
 
 	# Set the mob's position to the spawn location.
-	mob.position = mob_spawn_location.position
+	mob.position = mob_spawn_location.global_position
 	print("Here I am!")
 	
 	# Choose the velocity for the mob.
